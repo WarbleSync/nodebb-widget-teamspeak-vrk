59993fd3222e165a923d8ead4689962e4b9ce503
bwallace1231@gmail.com <bwallace1231@gmail.com>
2017-04-14 12:14:42 -0400

    base creation

--- library.js
+++ library.js
@@ -1,3 +1,33 @@
 (function(module) {
-  
+  'use strict';
+  var async = require('async'),
+  fs = require('fs'),
+	path = require('path'),
+	http = require('https'),
+	templates = module.parent.require('templates.js'),
+	app;
+
+  var Widget = {
+		templates: {}
+	};
+
+  Widget.init = function(params, callback) {
+
+  };
+
+  Widget.renderTeamspeakWidget = function(widget, callback) {
+
+  };
+
+  Widget.defineWidget = function(widgets, callback) {
+    widgets.push({
+    			widget: 'teamspeak-vrk',
+    			name: 'Teamspeak',
+    			description: 'Shows who is currently online in Teamspeak.',
+    			content: fs.readFileSync(path.resolve(__dirname, './public/templates/widget.tpl')),
+    		});
+    
+    		callback(null, widgets);
+  };
+
 }(module));
